8cf7a7422b34d11716c6f1f49efb91aeb4f53d00
DinaKhaled <dina.elghndour@gmail.com>
2021-02-16 20:13:45 +0200

    initial files

---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button as MButton } from "@material-ui/core";
+
+const Button = () => {
+  return (
+    <MButton variant="contained" color="primary">
+      Primary
+    </MButton>
+  );
+};
+
+export default Button;
