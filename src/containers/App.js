6cd246f37f4b39063add2a3d6f4a3a6b4146257c
DinaKhaled <dina.elghndour@gmail.com>
2021-02-16 21:12:39 +0200

    [1] layout drawer component added

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,13 +4,16 @@ import history from "../routes/History";
 import Routes from "../routes/Routes";
 import { ThemeProvider } from "@material-ui/core/styles";
 import Theme from "../utils/Theme";
+import Layout from "../components/Layout";
 // ========== General styles ==========
 import "./App.scss";
 
 const App = () => {
   return (
     <ThemeProvider theme={Theme}>
-      <Router history={history}>{Routes}</Router>
+      <Router history={history}>
+        <Layout>{Routes}</Layout>
+      </Router>
     </ThemeProvider>
   );
 };
