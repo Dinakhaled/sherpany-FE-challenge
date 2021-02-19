2b680ecf7f88d60f7de2564bbc79af0bfb805ab1
DinaKhaled <dina.elghndour@gmail.com>
2021-02-19 20:54:20 +0200

    [4] Add close icon to the modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { Dialog, Grid, List, ListItem, ListItemText } from "@material-ui/core";
+import {
+  Dialog,
+  Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
 import styles from "./Modal.module.scss";
 
 const Modal = ({ header, infos = [], isOpen = false, handleClose }) => {
@@ -10,7 +18,12 @@ const Modal = ({ header, infos = [], isOpen = false, handleClose }) => {
       fullWidth={true}
       maxWidth={"sm"}
     >
-      <div className={styles.header}>{header}</div>
+      <div className={styles.header}>
+        <div>{header}</div>
+        <IconButton aria-label="close" onClick={handleClose}>
+          <CloseIcon />
+        </IconButton>
+      </div>
       <List>
         <Grid container spacing={0} className="p-3">
           {infos.map((info, index) => (
